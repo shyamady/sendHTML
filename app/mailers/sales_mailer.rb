e6b076b690d6623f31f4b2a558ad26991d221757
yamada <sy@gociao.com>
2019-02-14 23:54:29 -0800

    Add mail setting

---
+++ app/mailers/sales_mailer.rb
@@ -0,0 +1,13 @@
+class SalesMailer < ApplicationMailer
+    default from: "noreply@example.com"
+
+    def send_sales(user)
+        @user = user
+        mail(
+            subject: "test"
+            to: @user.email
+        ) do |format|
+            format.text
+        end
+    end
+end
---
+++ test/mailers/sales_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SalesMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
